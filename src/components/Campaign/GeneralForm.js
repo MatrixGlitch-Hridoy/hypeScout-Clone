d2e5900eaa1badc57707c6abda1becdf8c303824
Hridoy <rkhridoy68@gmail.com>
2021-11-23 00:04:02 +0600

    form change

--- src/components/Campaign/GeneralForm.js
+++ src/components/Campaign/GeneralForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const GeneralForm = () => {
    
-    const [primaryOptionPaltform, setPrimaryOptionPlatform] = useState(false);
+   // const [primaryOptionPaltform, setPrimaryOptionPlatform] = useState(false);
     // const [one, setOne] = useState(true);
     // const [two, setTwo] = useState(false);
     // const [three, setThree] = useState(false);
@@ -14,50 +14,64 @@ const GeneralForm = () => {
   //let show=[true,false,false,false,false,false,false];
     //const[dispaly,setDisplay]=useState([true,false,false,false,false,false,false]);
     //const[show,setShow]=useState({0:true,1:false,2:false,3:false,4:false,5:false,6:false});
-    const[show,setShow]=useState([true,false,false,false,false,false,false]);
-    const handleOptionPlatform = (id) => {
-        let dispaly=[false,false,false,false,false,false,false];
-        //let dispaly=show;
-         // console.log('clicke platform',id);
-        let nextId=parseInt(id+1);
-       // console.log('next id',show);
-        dispaly[id]=!show[id];
-        //dispaly[id]=false;
-        //console.log('dipaly one',dispaly);
-        dispaly[`${nextId}`]=!show[`${nextId}`];
+    // const[show,setShow]=useState([true,false,false,false,false,false,false]);
+    // const handleOptionPlatform = (id) => {
+    //     let dispaly=[false,false,false,false,false,false,false];
+    //     //let dispaly=show;
+    //      // console.log('clicke platform',id);
+    //     let nextId=parseInt(id+1);
+    //    // console.log('next id',show);
+    //     dispaly[id]=!show[id];
+    //     //dispaly[id]=false;
+    //     //console.log('dipaly one',dispaly);
+    //     dispaly[`${nextId}`]=!show[`${nextId}`];
       
      
-       // show[id]=!show[id];
-        console.log('dipaly second',dispaly);
-        //show=dispaly;
-        setShow(dispaly);
+    //    // show[id]=!show[id];
+    //     console.log('dipaly second',dispaly);
+    //     //show=dispaly;
+    //     setShow(dispaly);
         
        
-    }
-    const handleOptionPlatformBack = (id) => {
-        let dispaly=[false,false,false,false,false,false,false];
-        //let dispaly=show;
-         // console.log('clicke platform',id);
-        let nextId=parseInt(id-1);
-       // console.log('next id',show);
-        dispaly[id]=!show[id];
-        //dispaly[id]=false;
-        //console.log('dipaly one',dispaly);
-        dispaly[`${nextId}`]=!show[`${nextId}`];
+    // }
+    // const handleOptionPlatformBack = (id) => {
+    //     let dispaly=[false,false,false,false,false,false,false];
+    //     //let dispaly=show;
+    //      // console.log('clicke platform',id);
+    //     let nextId=parseInt(id-1);
+    //    // console.log('next id',show);
+    //     dispaly[id]=!show[id];
+    //     //dispaly[id]=false;
+    //     //console.log('dipaly one',dispaly);
+    //     dispaly[`${nextId}`]=!show[`${nextId}`];
       
      
-       // show[id]=!show[id];
-        console.log('dipaly second',dispaly);
-        //show=dispaly;
-        setShow(dispaly);
+    //    // show[id]=!show[id];
+    //     console.log('dipaly second',dispaly);
+    //     //show=dispaly;
+    //     setShow(dispaly);
         
        
-    }
+    // }
+    const [form, setForm] = useState({
+        email: "",
+        name: "",
+        password: "",
+      })
+      const [count, setCount] = useState(1)
+    
+      const updateForm = (e) => {
+        setForm({
+          ...form,
+          [e.target.name]: e.target.value,
+        })
+      }
     return (
         <>  
-            {show[0]&& <div>
+            {
+            /* {show[0]&& <div>
             <h1>Platform1</h1>
-
+            
             <h2>What is your primary platform?</h2>
             <p>Primary platform where you want to run this campaign</p>
             <input type="text"/>
@@ -66,7 +80,7 @@ const GeneralForm = () => {
             }
             {show[1] && <div>
             <h1>Platform2</h1>
-
+            
             <h2>What is your primary platform?</h2>
             <p>Primary platform where you want to run this campaign</p>
             <input type="text"/>
@@ -133,7 +147,75 @@ const GeneralForm = () => {
             <input type="text"/>
             <div><button>next</button></div>
             </div>
+            } */
             }
+      <h1>Step {count} of 3</h1>
+      <form
+        className="col-4 form"
+        onSubmit={() =>
+          alert(
+            `Submitted Email: ${form.email} Name: ${form.name} Password: ${form.password}`
+          )
+        }
+      >
+        {count === 1 ? (
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              onChange={updateForm}
+              value={form.email}
+            />
+          </div>
+        ) : null}
+        {count === 2 ? (
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              onChange={updateForm}
+              value={form.name}
+            />
+          </div>
+        ) : null}
+        {count === 3 ? (
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              onChange={updateForm}
+              value={form.password}
+            />
+          </div>
+        ) : null}
+        {count === 3 ? (
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
+        ) : null}
+      </form>
+      <button
+        className="btn btn-dark"
+        type="submit"
+        onClick={() => setCount(count - 1)}
+        disabled={count < 2}
+      >
+        Back
+      </button>
+      <button
+        className="btn btn-light"
+        type="submit"
+        onClick={() => setCount(count + 1)}
+        disabled={count > 2}
+      >
+        Next
+      </button>
         </>
     );
 };
